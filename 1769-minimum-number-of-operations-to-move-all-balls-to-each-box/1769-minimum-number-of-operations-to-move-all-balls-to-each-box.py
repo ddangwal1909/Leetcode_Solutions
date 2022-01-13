2671152a471b31b20adc2d1ff7e9eb0eca3a24ef
Dhruv Dangwal <90011855+ddangwal1909@users.noreply.github.com>
2022-01-13 16:05:55 -0500

    Time: 7573 ms (7.54%), Space: 14.8 MB (22.87%) - LeetHub

---
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        
+        
+        
+        
+        '''
+        O(N^2) solution
+        ## initializing 2D array to keep track of operations
+        mat = [[0 for i in range(len(boxes))] for j in range(len(boxes))]
+        print(sum(mat[0]))
+        for i in range(len(boxes)):
+            for j in range(len(boxes)):
+                mat[i][j]=int(boxes[j])*abs(i-j)
+        
+        res=[]
+        for i in range(len(boxes)):
+            res.append(sum(mat[i]))
+        '''
+        
+        
+        nonzero_idx = [i for i in range(len(boxes)) if boxes[i]=='1']
+        
+        res=[]
+        for i in range(len(boxes)):
+            ops=0
+            for j in nonzero_idx:
+                ops+=int(boxes[j])*abs(i-j)
+            
+            res.append(ops)
+        return res
+        
