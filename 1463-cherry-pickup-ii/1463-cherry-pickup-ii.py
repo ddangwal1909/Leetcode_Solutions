3d1bb55dbeb845a831c85fede04b770f96bcef14
Dhruv Dangwal <90011855+ddangwal1909@users.noreply.github.com>
2022-01-16 15:12:34 -0500

    Time: 1121 ms (61.66%), Space: 19.5 MB (77.50%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -1,6 +1,27 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
+        
         rows=len(grid)
+        cols=len(grid[0])
+        
+        dp=[[[0 for i in range(cols+1)] for j in range(cols+1)] for k in range(rows+1)]
+        
+        
+        for i in range(rows-1,-1,-1):
+            for bot1 in range(0,cols):
+                for bot2 in range(bot1,cols):
+                    if bot1!=bot2:
+                        dp[i][bot1][bot2]=grid[i][bot1]+grid[i][bot2]+max([dp[i+1][bot1+i1][bot2+j1] for i1 in [-1,0,1] for j1 in [-1,0,1] ])
+                    else:
+                        dp[i][bot1][bot2]=grid[i][bot1]+max([dp[i+1][bot1+i1][bot2+j1] for i1 in [-1,0,1] for j1 in [-1,0,1] ])
+        
+        
+        res = dp[0][0][cols-1]
+        
+        return res
+        
+        
+        '''rows=len(grid)
         cols=len(grid[0])
         @lru_cache(None)
         def helper(bot1,bot2,row):
@@ -20,3 +41,6 @@ class Solution:
 
         res=helper(0,cols-1,0)
         return res
+        '''
+        
+        
