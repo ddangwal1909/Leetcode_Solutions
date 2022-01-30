d9ea1cfe60c1c629b19144901cd5e8efce0f61ad
Dhruv Dangwal <90011855+ddangwal1909@users.noreply.github.com>
2022-01-30 15:06:45 -0500

    Time: 10716 ms (10.30%), Space: 15.1 MB (71.26%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        dp_mn_steps=[0]*len(nums)
+        
+        for i in range(len(nums)):
+            for j in range(i+1,min(i+nums[i]+1,len(nums))):
+                if dp_mn_steps[j]!=0:
+                    dp_mn_steps[j]=min(dp_mn_steps[j],dp_mn_steps[i]+1)
+                else:
+                    dp_mn_steps[j]=dp_mn_steps[i]+1
+        
+        return dp_mn_steps[-1]
+                
