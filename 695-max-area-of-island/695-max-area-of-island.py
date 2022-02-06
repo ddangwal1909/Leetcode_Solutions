2fe40bcd65159c9221859f0ceb400c4da62968e5
Dhruv Dangwal <90011855+ddangwal1909@users.noreply.github.com>
2022-02-06 17:21:31 -0500

    Time: 2992 ms (5.00%), Space: 17.5 MB (27.28%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        
+        visited=[]
+        
+        mx=0
+        
+        def helper(i,j):
+            
+            tmp=grid[i][j]
+            for r,c in [(0,1),(1,0),(0,-1),(-1,0)]:
+                if (i+r)>=0 and (i+r)<len(grid) and (j+c)>=0 and (j+c)<len(grid[0]):
+                    if grid[i+r][j+c]==1 and (i+r,j+c) not in visited:
+                        visited.append((i+r,j+c))
+                        tmp = tmp+helper(i+r,j+c)
+            
+            return tmp
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (i,j) not in visited and grid[i][j]==1:
+                    visited.append((i,j))
+                    mx = max(mx,helper(i,j))
+        return mx
+                        
