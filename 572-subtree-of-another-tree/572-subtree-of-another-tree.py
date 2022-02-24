4ddaf7d702743aa850c162c3727d173014e33ab4
Dhruv Dangwal <90011855+ddangwal1909@users.noreply.github.com>
2022-02-23 20:21:37 -0500

    Time: 140 ms (80.00%), Space: 14.7 MB (94.48%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -10,12 +10,15 @@ class Solution:
     def dfs(self,root,subRoot):
         
         if root is None and subRoot is None:
+            ## matched till the end from root of both trees
             return True
+        
         elif (root is None and subRoot is not None) or (root is not None and subRoot is None):
             return False
         elif root.val != subRoot.val:
             return False
         else:
+            ## check if both and left and right match the conditions
             return self.dfs(root.left,subRoot.left) and self.dfs(root.right,subRoot.right)
     
     
@@ -25,14 +28,15 @@ class Solution:
         
         queue.append(root)
         
+        ## start Level Order BFS here
         while queue :
             
             tmp_queue = []
             
             while queue:
                 curr = queue.pop(0)
-                
                 if curr.val == subRoot.val:
+                    ## if did not find this time, try for next matched value of root.val
                     if self.dfs(curr,subRoot):
                         return True
                 
@@ -44,6 +48,7 @@ class Solution:
             
             queue = tmp_queue
         
+        ## even after all traversals the subtree wasnt found
         return False
             
         
