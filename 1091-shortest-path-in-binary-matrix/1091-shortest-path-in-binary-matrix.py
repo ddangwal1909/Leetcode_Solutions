e01a3d9ed7168f806110a4e317ba248b89305d1d
Dhruv Dangwal <90011855+ddangwal1909@users.noreply.github.com>
2022-02-24 20:20:58 -0500

    Time: 832 ms (60.76%), Space: 14.3 MB (92.26%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -3,12 +3,15 @@ class Solution:
         
         directions = [(-1,0),(1,0),(-1,-1),(1,1),(0,1),(0,-1),(-1,1),(1,-1)]
         
+        mx_r = len(grid)
+        mx_col = len(grid[0])
+        
         def getneighbours(i,j):
             
             lst = []
             
             for r,c in directions:
-                if 0<=i+r<len(grid) and 0<=j+c<len(grid[0]):
+                if 0<=i+r<mx_r and 0<=j+c<mx_col:
                     if grid[i+r][j+c]==0:
                         lst.append((i+r,j+c))
             
@@ -25,7 +28,7 @@ class Solution:
         while queue:
             curr = queue.pop(0)
             distance = grid[curr[0]][curr[1]]
-            if curr[0]==len(grid)-1 and curr[1]==len(grid[0])-1:
+            if curr[0]==mx_r-1 and curr[1]==mx_col-1:
                     return grid[curr[0]][curr[1]]   
             for row,col in getneighbours(curr[0],curr[1]):
                 grid[row][col]=distance+1
