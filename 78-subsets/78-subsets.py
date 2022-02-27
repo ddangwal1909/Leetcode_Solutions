57cdd0d7da82bb7fc09e4c7a77d40f000da507c1
Dhruv Dangwal <90011855+ddangwal1909@users.noreply.github.com>
2022-02-27 14:15:52 -0500

    Time: 39 ms (71.49%), Space: 14 MB (97.18%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,39 +1,22 @@
 class Solution:
-    
-    def givebinary(self,l,ln):
-        binary=[]
-        while l:
-            if l%2==0:
-                binary.append(0)
-            else:
-                binary.append(1)
-            l=l//2
-            
-        if len(binary)!=ln:
-            st=len(binary)
-            en=ln
-            #print(st,en)
-            for i in range(st,en):
-                binary.append(0)
-        binary=binary[::-1]
-        return binary
-    
-    
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        
         
         res=[]
         
-        for i in range((2**(len(nums)))):
-            tmp=[]
-            s=self.givebinary(i,len(nums)) 
-            print(s)
-            for j in range(len(s)):
-                if s[j] == 1:
-                    tmp.append(nums[j])
-                else:
-                    continue
+        def dfs(start,curr):
             
-            res.append(tmp)
+            res.append(curr[:])
+            
+            for i in range(start,len(nums)):
+                curr.append(nums[i])                
+                dfs(i+1,curr[:])
+                curr.pop(len(curr)-1)
+            
+            return
+        
+        dfs(0,[])
         
         return res
         
+            
