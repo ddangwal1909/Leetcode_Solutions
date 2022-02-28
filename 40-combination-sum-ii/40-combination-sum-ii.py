e991d8ff1b38ea7a6227fd35079f431d48ccbc0d
Dhruv Dangwal <90011855+ddangwal1909@users.noreply.github.com>
2022-02-28 01:55:18 -0500

    Time: 190 ms (10.62%), Space: 14 MB (60.72%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        
+        candidates.sort()
+        res=[]
+        def helper(start,curr,sm):
+            
+            if sm==target and curr[:] not in res:
+                res.append(curr[:])
+                return 
+            
+            if sm>target or start>=len(candidates):
+                return
+            
+            
+            
+            sm+=candidates[start]
+            curr.append(candidates[start])
+            helper(start+1,curr[:],sm)
+            sm-=candidates[start]
+            curr.pop(len(curr)-1)
+            
+            while start+1<len(candidates) and candidates[start]==candidates[start+1]:
+                start+=1
+            
+            helper(start+1,curr[:],sm)
+                
+            
+            return
+        
+        
+        helper(0,[],0)
+        
+        return res
