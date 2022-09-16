d1cdf6e046ac25d4e59a2012d1267bba2f626efe
Dhruv Dangwal <90011855+ddangwal1909@users.noreply.github.com>
2022-09-15 21:50:08 -0400

    Time: 1328 ms (5.09%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,22 +1,17 @@
 # Write your MySQL query statement below
 
 
+
+select a.product_id, coalesce(b.final_price,10) as price
+from
 (
-select product_id, new_price as price
-from(
-select 
-* , row_number() over (partition by product_id order by change_date desc) as rnk
-from 
-(
-select * from products 
-where change_date<='2019-08-16'
+select distinct product_id
+from products
 ) a
-)b
-where rnk=1
-)
-union
-(
-select distinct product_id,10 as price from products
-where product_id not in ( select distinct product_id from products 
-where change_date<='2019-08-16')
-)
+left join (select * from (
+select product_id, row_number() over (partition by product_id order by change_date desc) as rnk ,  new_price as final_price
+from products
+where change_date<='2019-08-16'
+)c where c.rnk=1 )b
+on a.product_id = b.product_id
+;
