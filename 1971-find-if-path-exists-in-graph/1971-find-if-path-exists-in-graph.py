8e545f30826ab11fe645f856b104bddabdeb102b
Dhruv Dangwal <90011855+ddangwal1909@users.noreply.github.com>
2022-09-22 13:30:57 -0400

    Time: 5715 ms (5.02%), Space: 300.5 MB (27.71%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -0,0 +1,49 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        
+        graph = {}
+        
+        for i in edges:
+            if i[0] not in graph.keys():
+                graph[i[0]]=[]
+            if i[1] not in graph.keys():
+                graph[i[1]]=[]
+            graph[i[0]].append(i[1])
+            graph[i[1]].append(i[0])
+        if len(edges)==0:
+            if source==destination:
+                return True
+            else:
+                return False
+        
+        visited=[0 for i in range(max(graph.keys())+1)]
+        
+        for i in range(len(visited)):
+            if i not in graph.keys():
+                visited[i]=1
+            
+        print(visited)
+        print(graph)
+        result=False
+        def helper(curr_node):
+            nonlocal result
+            nonlocal destination
+            nonlocal visited
+            nonlocal graph
+            print(curr_node)
+            visited[curr_node]=1
+            
+            if result==True:
+                return
+            elif curr_node==destination:
+                result = True
+                return
+            else:
+                for i in graph[curr_node]:
+                    if visited[i]==0:
+                        helper(i)
+            return
+        
+        helper(source)
+        
+        return result
