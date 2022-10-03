a3ad9d7cbef7b5d5cb5b2ed693302714f0262616
Dhruv Dangwal <90011855+ddangwal1909@users.noreply.github.com>
2022-10-03 12:44:16 -0400

    Time: 89 ms (40.34%), Space: 14 MB (5.69%) - LeetHub

---
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSumLessThanK(self, nums: List[int], k: int) -> int:
+        
+        nums.sort()
+        i=0
+        j=len(nums)-1
+        res=-1
+        while i<j:
+            if nums[i]+nums[j]<k:
+                res=max(res,nums[i]+nums[j])
+                i+=1               
+            else:
+                j-=1
+        return res
+            
+            
+        
